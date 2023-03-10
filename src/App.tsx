adad185b154c23267e2c7f29a8818d6e55e12ac2
Hima <violet@himaa.me>
2023-03-10 13:44:34 +0000

    update mobile first

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { config } from './config'
 export default function App() {
   return (
     <div>
-      <h1>MRT Map</h1>
+      <h1></h1>
       <Map apiUrl={config.VITE_API_URL} />
     </div>
   )
